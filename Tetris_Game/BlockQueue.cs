49028fa22b43fd35d86a4cb93591b92891313aff
Chirag Patil <patilchirag484@gmail.com>
2022-12-02 08:34:25 -0500

    BlockQueue class added with Random-block functionality

---
+++ Tetris_Game/BlockQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tetris_Game
+{
+    public class BlockQueue
+    {
+        public readonly Block[] blocks = new Block[] {
+           new IBlock(),
+           new JBlock(),
+           new  LBlock(),
+           new  OBlock(),
+           new SBlock(),
+           new TBlock(),
+           new ZBlock()
+
+        };
+        private readonly Random random= new Random();   
+        public Block NextBlock { get;private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+        public Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+            do{
+                NextBlock = RandomBlock();
+            } while (block.Id== NextBlock.Id);
+            return block;
+        } 
+    }
+}
